38c874e9e5b06dabf29680c18acf1d209413c668
Clayton Hart <claytonhart123@yahoo.com>
2019-10-18 15:24:16 -0700

    updated features styling

--- 1theme/src/App.js
+++ 1theme/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Masthead from "./components/Masthead";
+import Features from "./components/Features";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Masthead />
+      <Features />
     </div>
   );
 }
---
+++ 1theme/src/components/Features.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section className="features">
+      <div className="container">
+        <div className="features-item">
+          <i className="far fa-folder features-item__img"></i>
+          <h4 className="features-item__header">Fully Responsive</h4>
+          <p className="features-item__desc">
+            This theme will look great on any device, no matter the size!
+          </p>
+        </div>
+        <div className="features-item">
+          <i className="far fa-clipboard features-item__img"></i>
+          <h4 className="features-item__header">Bootstrap 4 Ready</h4>
+          <p className="features-item__desc">
+            Featuring the latest build of the new Bootstrap 4 framework!
+          </p>
+        </div>
+        <div className="features-item">
+          <i className="far fa-check-circle features-item__img"></i>
+          <h4 className="features-item__header">Easy to Use</h4>
+          <p className="features-item__desc">
+            Ready to use with your own content, or customize the source files!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
