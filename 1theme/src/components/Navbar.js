22745948022182cfaa8d44edd0cbde8c2cee9278
Clayton Hart <claytonhart123@yahoo.com>
2019-10-18 13:16:07 -0700

    navbar

---
+++ 1theme/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="navbar">
+      <div className="container">
+        <h4 className="navbar__title">Start Bootstrap</h4>
+        <button className="btn navbar__button">Sign In</button>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
